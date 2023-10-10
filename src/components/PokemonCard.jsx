7c010f49b4c066bc7c56c0cdf35dcd4ea7ab8466
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-10-10 12:00:31 +0200

    Add: my first PropTypes not easy?

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,13 @@
-
+import PropTypes from 'prop-types'
 
     const PokemonCard = (props) =>{
+      
+      PokemonCard.PropTypes = {
+        props: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc:PropTypes.string.isRequired
+        }).isRequip
+        }
         // const pokemon = pokemon[0];
         const {pokemon} = props
     return (
@@ -10,7 +17,9 @@
           <figcaption>{pokemon.name}</figcaption>
         </figure>
       </>
-        )
- };
+        );
+        
+  }
+ 
 
 export default PokemonCard
